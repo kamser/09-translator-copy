79fad3265ade23389261331c9a5a6f4430317eaf
kamser <keylor.morataya@gmail.com>
2026-05-30 15:50:18 -0700

    Created the internal function to modify the state with the dispatch and returned them for the custom hook

--- src/hooks/useTranslatorState.tsx
+++ src/hooks/useTranslatorState.tsx
@@ -1,5 +1,6 @@
 import { useReducer } from 'react'
 import { initialState, reducer } from '../reducer/translatorReducer.tsx'
+import { ACTION_TYPE } from '../reducer/actionTypes'
 
 export function useTranslatorState(){
     const [{
@@ -10,12 +11,37 @@ export function useTranslatorState(){
         loading
     }, dispatch] = useReducer(reducer, initialState)
 
+    const interchangeLanguage = (): void =>{
+        dispatch({type: ACTION_TYPE.INTERCHANGET_LANGUAGES})
+    }
+
+    const setFromLanguage = (selectedLanguage: string): void =>{
+        dispatch({type: ACTION_TYPE.SET_FROM_LANGUAGE, payload: selectedLanguage})
+    }
+
+    const setToLanguage = (targetlanguage: string): void =>{
+        dispatch({type: ACTION_TYPE.SET_TO_LANGUAGE, payload: targetlanguage})
+    }
+
+    const setFromText = (inputText: string): void =>{
+        dispatch({type: ACTION_TYPE.SET_FROM_TEXT, payload: inputText})
+    }
+
+    const setResult = (result: string): void =>{
+        dispatch({type: ACTION_TYPE.SET_RESULT, payload: result})
+    }
+
     return {
         fromLanguage,
         toLanguage,
         fromText,
         result,
-        loading
+        loading,
+        interchangeLanguage,
+        setFromLanguage,
+        setToLanguage,
+        setFromText,
+        setResult
     }
 }
  
