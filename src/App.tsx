4e77323207b48f446d45d820c25ff43b19558404
kamser <keylor.morataya@gmail.com>
2026-05-30 15:32:22 -0700

    Added a button to test the state with the dispatch actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { useReducer } from 'react'
 import { initialState, reducer } from './reducer/translatorReducer'
+import { ACTION_TYPE } from './reducer/actionTypes'
 
 function App() {
 
-  const [state, dispatch] = useReducer(initialState, reducer)
+  const [{
+    fromLanguage,
+    toLangueage,
+    fromText,
+    result,
+    loading
+  }, dispatch] = useReducer(reducer, initialState)
+
+  console.log(fromLanguage)
 
   return (
-    <>
+    <div className='App'>
       <h1>Best Translator</h1>
-    </>
+      <button
+        onClick={() => dispatch({type: ACTION_TYPE.SET_FROM_LANGUAGE, payload: 'es'})}
+      >Cambiar a Espa;ol</button>
+    </div>
   )
 }
 
