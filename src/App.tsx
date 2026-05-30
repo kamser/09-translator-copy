b0f15c2567d8088f0a1338fc4b4dd2fbfd5e9bc2
kamser <keylor.morataya@gmail.com>
2026-05-30 01:22:14 -0700

    Created the reducer file where the initial state and the basic reducer function was created. Then, the useReducer was imported in the App component to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css'
+import { useReducer } from 'react'
+import { initialState, reducer } from './reducer/translatorReducer'
 
 function App() {
+
+  const [state, dispatch] = useReducer(initialState, reducer)
 
   return (
     <>
---
+++ src/reducer/translatorReducer.tsx
@@ -0,0 +1,12 @@
+
+export const initialState = {
+    fromLanguage: 'auto',
+    toLanguage: 'en',
+    fromText: '',
+    result: '',
+    loading: false
+}
+
+export function reducer({state, action}){
+    
+}
