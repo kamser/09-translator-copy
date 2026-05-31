fd7f2f4d66b5ec1c8b5622932cd9e7b06515327c
kamser <keylor.morataya@gmail.com>
2026-05-30 17:25:02 -0700

    Finished the basic form for the traslator UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
 import './App.css'
 
 import { useTranslatorState } from './hooks/useTranslatorState.tsx'
 
 function App() {
-  const {setFromLanguage} = useTranslatorState()
+  const {fromLanguage, toLanguage, interchangeLanguage, setFromLanguage} = useTranslatorState()
   return (
-    <div className='App'>
+    <Container fluid>
       <h1>Best Translator</h1>
-      <button
-        onClick={() => setFromLanguage('es')}
-      >Cambiar a Espa;ol</button>
-    </div>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+        <Col>
+          <button onClick={interchangeLanguage}>
+            Interchange
+          </button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
