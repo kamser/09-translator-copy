b647715644d9fddda3e0c06433a8c6d9cf597a71
kamser <keylor.morataya@gmail.com>
2026-05-31 00:55:17 -0700

    Create the basic Text Area component and its props

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,24 @@
+import { Form } from "react-bootstrap";
+
+import { SectionType } from "../types/types";
+
+interface Props {
+    type: SectionType
+    placeholder: string
+    loading?: boolean
+    onChange: (value: string) => void
+    value: string
+}
+
+export function TextArea({type, placeholder, loading, value, onChange}: Props){
+    return(
+        <Form.Control
+            autoFocus={type === SectionType.From}
+            as='textarea'
+            placeholder={placeholder}
+            style={{height: '150px'}}
+        >
+
+        </Form.Control>
+    )
+}
