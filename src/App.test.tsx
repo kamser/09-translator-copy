44b19deaed08ec93051bf96e578da26c8537c786
kamser <keylor.morataya@gmail.com>
2026-06-12 23:52:17 -0700

    Added the import to trigger a user event on the test and set the user before the app rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import {test, expect} from 'vitest'
 import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 test('My App works as expected', () => {
+    const user = userEvent.setup()
     const app = render(<App/>)
 
     const textareaForm = app.getByPlaceholderText('Type some text')
