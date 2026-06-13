ca8e45d167dabd81e65bebd56efa719861c5f829
kamser <keylor.morataya@gmail.com>
2026-06-13 00:02:26 -0700

    Completed the test and added the command to use the npm run

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,14 @@ import {render} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('My App works as expected', () => {
+test('My App works as expected', async () => {
     const user = userEvent.setup()
     const app = render(<App/>)
 
     const textareaForm = app.getByPlaceholderText('Type some text')
+
+    await user.type(textareaForm, 'Hola mundo')
+    const result = await app.findByDisplayValue(/Hello world/i, {}, {timeout: 10000})
+
+    expect(result).toBeTruthy()
 })
